0cf6599f1279a6f497abc8d24cf4befd474a59c9
yura <yurarozhko.moa@gmail.com>
2019-11-30 14:38:04 +0200

    refactor Users package

---
+++ src/components/Users/Users.js
@@ -0,0 +1,61 @@
+import React from 'react';
+import s from "./Users.module.css";
+import userPhoto from "../../assets/images/qq.jpg";
+
+
+let Users = (props) => {
+    let pagesCount = Math.ceil(props.totalUsersCount / props.pageSize);
+
+    let pages = [];
+    for (let i = 1; i <= pagesCount; i++) {
+        pages.push(i);
+    }
+    return (
+        <div>
+            <div className={s.render}>
+                <div>
+                    {props.users.map(u => <div key={u.id}>
+                    <span>
+                        <div className={s.photo}>
+                            <img src={u.photos.small != null ? u.photos.small : userPhoto} alt={"user"}/>
+                        </div>
+                        <div>
+                            {u.followed
+                                ? <button onClick={() => {
+                                    props.unfollow(u.id)
+                                }}>Follow</button>
+                                : <button onClick={() => {
+                                    props.follow(u.id)
+                                }}>UnFollow</button>
+                            }
+                        </div>
+                    </span>
+                        <span>
+                        <span>
+                            <div>{u.name}</div>
+                            <div>{u.status}</div>
+                        </span>
+                            <div>{"u.location.city"}</div>
+                            <div>{"u.location.country"}</div>
+                        <span>
+
+                        </span>
+                    </span>
+                    </div>)}
+                    <div className={s.pageNumber}>
+                        {pages.map(p => {
+                            return <div
+                                className={props.currentPage === p && s.selectedPage}
+                                onClick={(e) => {
+                                    props.onPageChanged(p);
+                                }}>
+                                |{p}</div>
+                        })}
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+};
+
+export default Users;
